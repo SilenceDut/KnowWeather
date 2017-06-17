21e650f79699b50b35bba806e8f71cda6072d64c
SilenceDut <silencedutchman@foxmail.com>
2017-06-17 16:53:36 +0800

    fix bug

--- app/src/main/java/com/silencedut/knowweather/scheduler/TaskCallback.java
+++ app/src/main/java/com/silencedut/knowweather/scheduler/TaskCallback.java
@@ -9,7 +9,7 @@ public interface TaskCallback{
 
     abstract class Callback<R> {
         public Type rType = ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
-        abstract void onSuccess(R response);
+        public abstract void onSuccess(R response);
         abstract void onError(ErrorBundle error);
     }
 
